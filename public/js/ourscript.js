6f37fdac3f337b9113dbbc284eee50e53cfe78d1
Jeriel <jeadarquahyiadom@davidson.edu>
2019-07-11 21:41:58 -0700

    sherif's magic

--- public/js/ourscript.js
+++ public/js/ourscript.js
@@ -2,8 +2,10 @@
 var video = document.getElementById('video');
 
 // Get access to the camera!
+// Props to David Walsh 
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
+    console.log("The code is running")
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         video.srcObject = stream;
         video.src = window.URL.createObjectURL(stream);
@@ -11,28 +13,28 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     });
 }
 
-// Legacy code below: getUserMedia 
-else if(navigator.getUserMedia) { // Standard
-    navigator.getUserMedia({ video: true }, function(stream) {
-        video.src = stream;
-        video.play();
-    }, errBack);
-} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-    navigator.webkitGetUserMedia({ video: true }, function(stream){
-        video.src = window.webkitURL.createObjectURL(stream);
-        video.play();
-    }, errBack);
-} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
-    navigator.mozGetUserMedia({ video: true }, function(stream){
-        video.srcObject = stream;
-        video.play();
-    }, errBack);
-}
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var video = document.getElementById('video');
+// // Legacy code below: getUserMedia 
+// else if(navigator.getUserMedia) { // Standard
+//     navigator.getUserMedia({ video: true }, function(stream) {
+//         video.src = stream;
+//         video.play();
+//     }, errBack);
+// } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+//     navigator.webkitGetUserMedia({ video: true }, function(stream){
+//         video.src = window.webkitURL.createObjectURL(stream);
+//         video.play();
+//     }, errBack);
+// } else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+//     navigator.mozGetUserMedia({ video: true }, function(stream){
+//         video.srcObject = stream;
+//         video.play();
+//     }, errBack);
+// }
+// var canvas = document.getElementById('canvas');
+// var context = canvas.getContext('2d');
+// var video = document.getElementById('video');
 
-// Trigger photo take
-document.getElementById("snap").addEventListener("click", function() {
-	context.drawImage(video, 0, 0, 640, 480);
-});
+// // Trigger photo take
+// document.getElementById("snap").addEventListener("click", function() {
+// 	context.drawImage(video, 0, 0, 640, 480);
+// });
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
-const { User, Closet } = require("../models");
+const { db, User } = require("../models");
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
@@ -11,23 +11,29 @@ router.get("/", function(req, res, next) {
 });
 
 router.get("/register", function(req, res, next) {
-  res.render("register")
+  res.render("register");
+});
+
+router.post("/register", function(req, res, next) {
+  const name = req.body.name;
+  const email = req.body.email;
+  db.registerNewUser({ name, email });
+  req.session.username = name;
+  res.redirect("/")
 });
 
 router.post("/login", function(req, res, next) {
   let username = req.body.username;
 
   User.findByLogin(username).then(user => {
-    if(user) {
-      req.session.username = username
-      res.redirect("/")
+    if (user) {
+      req.session.username = username;
+      req.session.userID = user._id
+      res.redirect("/");
     } else {
-      res.send(`${username} not found`)
+      res.send(`${username} not found`);
     }
   });
 });
-
-
-
 
 module.exports = router;
